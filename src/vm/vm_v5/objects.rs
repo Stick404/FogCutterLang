2342673b7daa49cae3c08e1a7ec672061b746421
kaii <amkaii@noreply.codeberg.org>
2026-04-11 23:14:40 +0200

    obj_ref and obj_val
    a suggestion on how they could be implemented using Smart Pointers
    They're described well in the book's chapter 15.
    happy hacking

--- src/vm/vm_v5/objects.rs
+++ src/vm/vm_v5/objects.rs
@@ -1,3 +1,5 @@
+use std::{cell::RefCell, rc::Rc};
+
 use super::vm_state::VmError;
 // use super::vm_state::VmResult;
 
@@ -7,6 +9,7 @@ pub enum PassBy {
 	Reference, // Returns the pointer
 }
 
+#[allow(non_camel_case_types)]
 #[derive(PartialEq, Debug, Clone)]
 pub enum Object {
 	BOOLEAN(bool),
@@ -22,9 +25,8 @@ pub enum Object {
 	FLOAT(f32),
 	DOUBLE(f64),
 
-	// TODO: Fuck. Meta Objects
-	//OBJECT_REFERNCE { types: Vec<&Object> },
-	//OBJECT_VALUE { types: Vec<&Object> },
+	OBJECT_REFERNCE(Rc<RefCell<Object>>),
+	OBJECT_VALUE(Box<Object>),
 
 	// UTF16 based strings
 	STRING(Vec<u16>),
@@ -49,7 +51,7 @@ impl TryFrom<Object> for bool {
 			SLONG(x) => Ok(x == 0),
 			FLOAT(x) => Ok(x == 0.0),
 			DOUBLE(x) => Ok(x == 0.0),
-			STRING(x) => Ok(x.len() != 0),
+			STRING(x) => Ok(!x.is_empty()),
 			_ => Err((0, "Object::castToBool")),
 		}
 	}
